a5cd3b899eadffc6943fbed564ebb438163f3924
Dan Engel <dengel29@gmail.com>
2021-03-11 14:27:01 +0800

    changed upload folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,39 +1,37 @@
-module.exports = config => {
+module.exports = (config) => {
   config.setUseGitIgnore(false);
-
+  config.addPassthroughCopy("./src/admin");
   // Transforms
-  const htmlMinTransform = require('./src/transforms/html-min-transform.js');
+  const htmlMinTransform = require("./src/transforms/html-min-transform.js");
 
   // Create a helpful production flag
-  const isProduction = process.env.NODE_ENV === 'production';
+  const isProduction = process.env.NODE_ENV === "production";
 
   // collections
-  config.addCollection('blog', collection => {
-    return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
-  })
-  config.addCollection('notes', collection => {
-    return [...collection.getFilteredByGlob('./src/notes/*.md')].reverse();
-  })
-  config.addCollection('projects', collection => {
-    let projects = [...collection.getFilteredByGlob('./src/projects/*.md')];
+  config.addCollection("blog", (collection) => {
+    return [...collection.getFilteredByGlob("./src/posts/*.md")].reverse();
+  });
+  config.addCollection("notes", (collection) => {
+    return [...collection.getFilteredByGlob("./src/notes/*.md")].reverse();
+  });
+  config.addCollection("projects", (collection) => {
+    let projects = [...collection.getFilteredByGlob("./src/projects/*.md")];
     projects.sort((a, b) => a.data.order - b.data.order);
-    return projects
-  })
-
+    return projects;
+  });
 
   // Only minify HTML if we are in production because it slows builds _right_ down
   if (isProduction) {
-    config.addTransform('htmlmin', htmlMinTransform);
+    config.addTransform("htmlmin", htmlMinTransform);
   }
 
   return {
-    markdownTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
     dir: {
-      input: 'src',
-      output: 'dist'
-    }
+      input: "src",
+      output: "dist",
+    },
   };
 };
-
